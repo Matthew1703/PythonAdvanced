29d8e7f0eb7efbc994d28e9dc37d599483a62715
Matvey Bachurin <matvey_1703@mail.ru>
2023-03-15 20:19:11 +0500

    Делаю 2 тему. Часть 2. Доделал.

--- mod2/7.py
+++ mod2/7.py
@@ -5,20 +5,27 @@ app = Flask(__name__)
 storage = {}
 
 @app.route("/add/<date>/<int:number>")
-def addDay(date, number) -> str:
-    day = date[6:8]
-    month = date[4:6]
-    year = date[:4]
-    return f"{day}"
+def addDay(date: str, number: int) -> str:
+    day = int(date[6:8])
+    month = int(date[4:6])
+    year = int(date[:4])
+    global storage
+    storage.setdefault(year, {}).setdefault(month, {}).setdefault(day, 0)
+    storage[year][month][day] += number
+    return f"Дата: {year}-{month}-{day} <br>Траты: {str(storage[year][month][day])}"
 
+@app.route("/calculate/<int:year>")
+def calculYear(year: int) -> str:
+    global storage
+    summ = 0
+    for value in storage[year].values():
+        summ += sum(value.values())
+    return f"Траты за год({year}): {str(summ)}"
 
-# @app.route("/calculate/<int:year>")
-# def calcul() -> str:
-#     pass
-#
-# @app.route("/calculate/<int:year>/<int:month>")
-# def calcul() -> str:
-#     pass
+@app.route("/calculate/<int:year>/<int:month>")
+def calculMonth(year: int, month: int) -> str:
+    global storage
+    return f"Траты за месяц({year}-{month}): {str(sum(storage[year][month].values()))}"
 
 if __name__ == "__main__":
     app.run(debug=True)
