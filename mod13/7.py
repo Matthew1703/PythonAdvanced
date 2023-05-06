4fda4b8a09a23e93bac74e02bb17b87d3a3beab8
Matvey Bachurin <matvey_1703@mail.ru>
2023-05-06 20:03:57 +0500

    Сделал 16 тему + поправки в 13 теме

--- mod13/7.py
+++ mod13/7.py
@@ -1,23 +1,27 @@
 import sqlite3
 
+
 def register(username: str, password: str) -> None:
     with sqlite3.connect('hw.db') as conn:
         cursor = conn.cursor()
         cursor.executescript(
             f"""
-            INSERT INTO `table_users` (username, password)
-            VALUES ('{username}, {password}')  
+                INSERT INTO 'table_users' (username, password)
+                    VALUES ('{username}', '{password}')  
             """
         )
         conn.commit()
+
+
 def hack() -> None:
-    # username: str = "i_like --  DELETE FROM 'table_users'"
-    # username: str = "i_like"
-    # username: str = "DELETE table_users"
-    username: str = "i_like', 1000) -- DELETE FROM 'table_users'"
-    # username: str = "i_like', 1000) -- "
-    password: str = "sql_injection"
+    username = "username"
+    password = "'); DELETE FROM table_users; --"
     register(username, password)
 
-if __name__ == "__main__":
-    hack()
+    data = str([('wrong_username' + str(i), 'wrong_password' + str(i)) for i in range(100)])[1:-1]
+    username = "username"
+    password = f"password'); INSERT INTO table_users (username, password) VALUES {data}; --"
+    register(username, password)
+
+
+hack()
